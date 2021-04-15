fd95f26d3a7b23d4f4c7781a6ddeb8871492ee8c
hehai <seaside5648@163.com>
2021-04-15 18:48:57 +0800

    添加 dataway 模块 实现不写代码提供接口

---
+++ hasor-dataway-demo/src/test/java/com/lee/hasordatawaydemo/HasorDatawayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.hasordatawaydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HasorDatawayDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
