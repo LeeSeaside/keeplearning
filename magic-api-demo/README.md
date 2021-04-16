cc26a7bdad3f81e28c13b89049f32359854047f6
hehai <seaside5648@163.com>
2021-04-16 14:04:27 +0800

    添加 magic-api 模块，完成基本配置

---
+++ magic-api-demo/README.md
@@ -0,0 +1,2 @@
+# magic-api-demo
+> 参考资料：[magic-api 官方文档](https://ssssssss.org/)
---
+++ magic-api-demo/src/test/java/com/lee/magicapidemo/MagicApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.magicapidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicApiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
